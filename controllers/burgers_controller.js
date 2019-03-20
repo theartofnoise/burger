0bd1c84e66ffa84dfc5d16b4553fabbafddfa94d
Art G <theartofnoise@gmail.com>
2019-03-20 18:41:40 -0400

    made ajax

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -31,12 +31,13 @@ router.post("/api/burgers", function(req, res) {
 });
 
 router.put("/api/burgers/:id", function(req, res) {
+    console.log("put req: "+req.params)
   var condition = "id = " + req.params.id;
 
-  console.log("condition", condition);
+  console.log("condition: "+ condition);
 
   burger.update({
-    eaten: req.body.eaten
+    eaten: true
   }, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
@@ -44,21 +45,22 @@ router.put("/api/burgers/:id", function(req, res) {
     } else {
       res.status(200).end();
     }
+    res.redirect('/');
   });
 });
 
-router.delete("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+// router.delete("/api/burgers/:id", function(req, res) {
+//   var condition = "id = " + req.params.id;
 
-  burger.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
+//   burger.delete(condition, function(result) {
+//     if (result.affectedRows == 0) {
+//       // If no rows were changed, then the ID must not exist, so 404
+//       return res.status(404).end();
+//     } else {
+//       res.status(200).end();
+//     }
+//   });
+// });
 
 // Export routes for server.js to use.
 module.exports = router;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (burger, eaten) VALUES ('Big Boy', false);
 INSERT INTO burgers (burger, eaten) VALUES ('Lil Boy', false);
-INSERT INTO burgers (burger, eaten) VALUES ('Nasty Boy', false);
+INSERT INTO burgers (burger, eaten) VALUES ('Nasty Boy', true);
