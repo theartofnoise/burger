c0abf65ac93e50cab1bb55207154cf0f04c091a0
Art G <theartofnoise@gmail.com>
2019-03-22 12:42:40 -0400

    ignore file pimped

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var password = process.env.PASSWORD;
 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "root",
+  password: password,
   database: "burgers_db"
 });
 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 
-var PORT = process.env.PORT;
+var PORT = process.env.PORT || 8080;
 
 var app = express();
 
