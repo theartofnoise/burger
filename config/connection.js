9b9384a694c8de3f214d3744799c23bab40263e0
Art G <theartofnoise@gmail.com>
2019-03-22 15:04:37 -0400

    db added

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 var password = process.env.PASSWORD;
 var connection;
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSEB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 connection = mysql.createConnection({
   host: "localhost",
